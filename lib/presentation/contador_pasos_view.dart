bc19a1e210bde6eb08b0db6115a9c0ec41bb534e
alexmarpan <alexmarpan84@gmail.com>
2023-01-06 19:28:48 +0100

    create step counter in file

---
+++ lib/presentation/contador_pasos_view.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:pedometer/pedometer.dart';
+
+class ContadorPasosView extends StatefulWidget {
+  const ContadorPasosView({super.key});
+
+  @override
+  State<ContadorPasosView> createState() => _ContadorPasosViewState();
+}
+
+class _ContadorPasosViewState extends State<ContadorPasosView> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'Steps taken:',
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            _steps,
+            style: const TextStyle(fontSize: 60),
+          ),
+          const Divider(
+            height: 100,
+            thickness: 0,
+            color: Colors.white,
+          ),
+          const Text(
+            'Pedestrian status:',
+            style: TextStyle(fontSize: 30),
+          ),
+          Icon(
+            _status == 'walking'
+                ? Icons.directions_walk
+                : _status == 'stopped'
+                    ? Icons.accessibility_new
+                    : Icons.error,
+            size: 100,
+          ),
+          Center(
+            child: Text(
+              _status,
+              style: _status == 'walking' || _status == 'stopped'
+                  ? const TextStyle(fontSize: 30)
+                  : const TextStyle(fontSize: 20, color: Colors.red),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  late Stream<StepCount> _stepCountStream;
+  late Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '?';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  void onStepCount(StepCount event) {
+    //print(event);
+    setState(() {
+      _steps = event.steps.toString();
+    });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    //print(event);
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    //print('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    //print(_status);
+  }
+
+  void onStepCountError(error) {
+    //print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  void initPlatformState() {
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+    _pedestrianStatusStream
+        .listen(onPedestrianStatusChanged)
+        .onError(onPedestrianStatusError);
+
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
+}
